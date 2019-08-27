f5c52006d5db66df1abcc115fde4e900310e4e53
Ravi Nada <ravi.nada@mountblue.io>
2019-08-27 12:15:47 +0000

    Update feature 1

--- Que_Ans.java
+++ Que_Ans.java
@@ -6,27 +6,27 @@ import java.util.HashMap;
 public class Que_Ans{
     static ArrayList<ArrayList<String>> getMatchesPlayedPerYear(ArrayList<ArrayList<String>> matchesData){
 
-        ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> result1 = new ArrayList<ArrayList<String>>();
 
-        HashMap<String,Integer> map = new HashMap<>();
+        HashMap<String,Integer> map1 = new HashMap<>();
 
         for(ArrayList<String> list : matchesData){
             String year = list.get(1);
-            if(map.containsKey(year)){
-                map.put(year,map.get(year)+1);  //check the year value if available than add or if not than put in to map
+            if(map1.containsKey(year)){
+                map1.put(year,map1.get(year)+1);  //check the year value if available than add or if not than put in to map
                 continue;
             }
-            map.put(year,1);
+            map1.put(year,1);
         }
 
-        for(String year : map.keySet()){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(year);      //year is added
-            list.add(map.get(year)+"");  //value added as per year
-            result.add(list);  //add final list in result map
+        for(String year : map1.keySet()){
+            ArrayList<String> list1 = new ArrayList<>();
+            list1.add(year);      //year is added
+            list1.add(map1.get(year)+"");  //value added as per year
+            result1.add(list1);  //add final list in result map
         }
 
-        return result;
+        return result1;
     }
 
 }
